38b11cb3b82d16c956c8cd7e1d113a4c660c98fe
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-02 17:39:20 +0100

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-container-external">
+    <div class="app-container-internal container-fluid px-0">
+      <HeaderComponent />
+      <HomePage />
+      <!--<router-view/>-->
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import HomePage from './views/HomePage.vue';
+export default { 
+    components: {
+    HeaderComponent,
+    HomePage
+  }
+}
+
+</script>
+
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+ul {
+    list-style: none;
+}
+a {
+    text-decoration: none;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.app-container-external {
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/router'
+
+createApp(App).use(router).mount('#app')
+
+
