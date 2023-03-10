f692db342ca2c1c76992ddb7be1a1f365a811d32
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-10 19:14:03 +0100

    cambio ordine menu

--- src/components/header/NavbarComponent.vue
+++ src/components/header/NavbarComponent.vue
@@ -14,8 +14,8 @@
             Il Distretto
           </a>
           <ul class="dropdown-menu">
-            <li><router-link class="dropdown-item" to="/comuni">I Comuni</router-link></li>
             <li><a class="dropdown-item" href="#">Piano di Zona</a></li>
+            <li><router-link class="dropdown-item" to="/comuni">I Comuni</router-link></li>
           </ul>
         </li>
 
