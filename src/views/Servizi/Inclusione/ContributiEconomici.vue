df24acf50e1ab96a4e737117e38da09e2a96952b
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-17 18:58:55 +0100

    aggiornamento view contributi economici

--- src/views/Servizi/Inclusione/ContributiEconomici.vue
+++ src/views/Servizi/Inclusione/ContributiEconomici.vue
@@ -1,23 +1,43 @@
 <template>
     <div>
-
-        <h1>Supporto Economico per disagi psichici</h1>
-        <p>
-          Il supporto ha carattere temporaneo ed avviene per il tempo e nelle misure economiche determinate dal programma terapeutico-riabilitativo, il contributo può avere diverse forme:
-        </p>
-        <ul style="list-style-type: disc;">
-          <li>assegno straordinario: ha carattere di urgenza ed è finalizzato a fronteggiare situazioni eccezionali in particolare per agevolare l’avvio del processo terapeutico;</li>
-          <li>assegno di emergenza temporanea;</li>
-          <li>assegno ordinario: fa parte del programma terapeutico ed è proposto dall’equipe curante, è corrisposto per un periodo di un anno, ed è rinnovabile;</li>
-          <li>assegno di reinserimento sociale: è finalizzato al reinserimento sociale o alla deistituzionalizzazione; è proposto dall’equipe curante, nei casi in cui l’utente non disponga di mezzi economici e di un valido supporto familiare, è corrisposto per un periodo di un anno, ed è rinnovabile.</li>
-        </ul>
-        <ul class="px-0">
-          <li>Dove:</li>
-          <li>Orari: </li>
-          <li>Chiama: </li>
-          <li>Come raggiungerci</li>
-        </ul>
-
+      <section>
+          <h1>Supporto Economico per disagi psichici</h1>
+          <p>
+            Il supporto ha carattere temporaneo ed avviene per il tempo e nelle misure economiche determinate dal programma terapeutico-riabilitativo, il contributo può avere diverse forme:
+          </p>
+          <ul style="list-style-type: disc;">
+            <li>assegno straordinario: ha carattere di urgenza ed è finalizzato a fronteggiare situazioni eccezionali in particolare per agevolare l’avvio del processo terapeutico;</li>
+            <li>assegno di emergenza temporanea;</li>
+            <li>assegno ordinario: fa parte del programma terapeutico ed è proposto dall’equipe curante, è corrisposto per un periodo di un anno, ed è rinnovabile;</li>
+            <li>assegno di reinserimento sociale: è finalizzato al reinserimento sociale o alla deistituzionalizzazione; è proposto dall’equipe curante, nei casi in cui l’utente non disponga di mezzi economici e di un valido supporto familiare, è corrisposto per un periodo di un anno, ed è rinnovabile.</li>
+          </ul>
+          <ul class="px-0">
+            <li>Dove:</li>
+            <li>Orari: </li>
+            <li>Chiama: </li>
+            <li>Come raggiungerci</li>
+          </ul>
+        </section>
+        <section>
+          <h1>Supporto Economico disagio alloggiativo</h1>
+          <p>
+            L’intervento mira al superamento di situazioni di emergenza abitativa. <br>
+            È rivolto ai cittadini (singoli o nuclei familiari) residenti nei Comuni di Anzio e Nettuno da almeno due anni, con reddito ISEE inferiore o uguale ad € 8.000,00 appartenenti ad una delle seguenti categorie:
+          </p>
+          <ul style="list-style-type: disc;">
+            <li>persone anziane e/o non autosufficienti;</li>
+            <li>singoli o nuclei familiari soggetti a procedura di sfratto esecutivo o di altra procedura esecutiva per il rilascio dell'abitazione;</li>
+            <li>singoli o nuclei familiari che dispongano di un reddito ISEE inferiore al valore del canone di locazione e che trovandosi quindi, nell'impossibilità oggettiva di pagare l'affitto dell'alloggio incorrano in situazione di “morosità incolpevole” e di avvio delle procedure esecutive di rilascio;</li>
+            <li>singoli o nuclei familiari che si trovano ad affrontare eventi di straordinaria emergenza;</li>
+            <li>singoli o nuclei familiari in condizioni di fragilità sociale ed a rischio di emarginazione.</li>
+          </ul>
+          <ul class="px-0">
+            <li>Dove:</li>
+            <li>Orari: </li>
+            <li>Chiama: </li>
+            <li>Come raggiungerci</li>
+          </ul>
+        </section>
       </div>
 </template>
 <script>
