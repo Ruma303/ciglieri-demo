aadc6d62b3f24ca553b53386e051bbc38e9420e6
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-24 11:29:07 +0100

    prova menu collassati all'inizio

--- src/views/AmministrazionePage.vue
+++ src/views/AmministrazionePage.vue
@@ -13,11 +13,11 @@
         <div class="accordion container-fluid" id="accordionPanelsStayOpenExample">
           <div class="accordion-item">
             <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-              <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="false" aria-controls="panelsStayOpen-collapseOne">
                 <h4>Disposizioni Generali</h4>
               </button>
             </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
               <div class="accordion-body">
                 <ul>
                   <li><a href="#">Link</a></li>
