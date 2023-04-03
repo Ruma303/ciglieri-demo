21578ca82ae288d510060ad71b40b8ddf6469cb8
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-04-03 16:27:20 +0200

    cambio PUA
    aggiunti nuovi dati e poster

--- src/views/Servizi/Ricevi aiuto/PuntoUnicoAccesso.vue
+++ src/views/Servizi/Ricevi aiuto/PuntoUnicoAccesso.vue
@@ -6,12 +6,31 @@
       <div class="poster-div">
         <img src="@/assets/img/locandina-pua.png" alt="locandina-pua" id="locandina-pua">
       </div>
-      <ul class="px-0">
+      <p>
+        Il PUA è la porta d’accesso al sistema territoriale dei servizi socioassistenziali e sociosanitari, tutta la popolazione può rivolgersi al PUA per ricevere accoglienza, ascolto e orientamento.
+      </p>
+
+      <p>
+        Il Punto Unico d’Accesso è aperto il lunedì, il mercoledì e  venerdì dalle 9:00 alle 12:00, il martedì e il giovedì dalle 9:00 alle 12:00 e dalle 15:00 alle 17:00, senza appuntamento.
+      </p>
+
+      <h3>PUA ASL Roma 6 - Anzio</h3>
+      <p> Via Aldobrandini n. 32 Villa Albani, Padiglione Sabatucci, stanza 5</p>
+      <p>Tel. 96 93 27 64 57</p>
+      <h3>PUA Comune di Anzio</h3>
+      <p>Viale Claudio Paolini n.6, Villa Adele – Servizi Sociali</p>
+      <p>Tel. 96 98 499419</p>
+      <h3>PUA Comune di Nettuno</h3>
+      <p>Viale della Vittoria n. 2, Servizi Sociali</p>
+      <p>Tel. 0698889555</p>
+
+      <!--! forse da eliminare -->
+      <!--<ul class="px-0">
         <li>Dove</li>
         <li>Orari</li>
         <li>Chiama</li>
         <li>Come raggiungerci</li>
-      </ul>
+      </ul>-->
     </div>
 </template>
 <script>
@@ -23,6 +42,7 @@ export default {
 <style lang="scss" scoped>
 #locandina-pua {
   min-width: 25rem;
-  width: 60%;
+  width: 70%;
+  margin: 2rem 0;
 }
 </style>
